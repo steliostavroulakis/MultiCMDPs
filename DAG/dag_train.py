1d9c2838b21fbbfc9f979b642975d9874316c828
RogerMin <rogermingaming@gmail.com>
2023-05-12 03:51:37 +0800

    Make dag_train importable

--- DAG/dag_train.py
+++ DAG/dag_train.py
@@ -35,113 +35,114 @@ use_max_lambda = False
 primal_step = 0.005
 dual_step = 0.1
 
-for knob in knobs:
+if __name__ == "__main__":
+    for knob in knobs:
 
-    players = dict()
-    for i, player_name in enumerate(player_names):
-        players[player_name] = Player(player_name, G, player_constrains[i], player_max_lambdas[i])
-    # players['Alice'] = Player('Alice',G)
-    # players['Bob'] = Player('Bob',G)
-    # players['Charlie'] = Player('Charlie',G)
+        players = dict()
+        for i, player_name in enumerate(player_names):
+            players[player_name] = Player(player_name, G, player_constrains[i], player_max_lambdas[i])
+        # players['Alice'] = Player('Alice',G)
+        # players['Bob'] = Player('Bob',G)
+        # players['Charlie'] = Player('Charlie',G)
 
-    #print(players['Charlie'].strategy)
+        #print(players['Charlie'].strategy)
 
-    #sys.exit(0)
+        #sys.exit(0)
 
-    # plt_kl_Alice = []
-    # plt_kl_Bob = []
-    # plt_kl_Charlie = []
+        # plt_kl_Alice = []
+        # plt_kl_Bob = []
+        # plt_kl_Charlie = []
 
-    # plt_wass_Alice = []
-    # plt_wass_Bob = []
-    # plt_wass_Charlie = []
+        # plt_wass_Alice = []
+        # plt_wass_Bob = []
+        # plt_wass_Charlie = []
 
-    alice_str_over_time = []
-    bob_str_over_time = []
-    charlie_str_over_time = []
+        alice_str_over_time = []
+        bob_str_over_time = []
+        charlie_str_over_time = []
 
-    iterates = 15000
-    lamda = [0]
-    total_gas_bound = knob
+        iterates = 15000
+        lamda = [0]
+        total_gas_bound = knob
 
-    for i in range(iterates):
-        print(f"Starting Iteration {i}/{iterates}")
-        
-        gradient_descent_ascent(G,players, lamda, total_gas_bound, use_max_lambda=use_max_lambda,
-                                primal_step=primal_step, dual_step=dual_step)
+        for i in range(iterates):
+            print(f"Starting Iteration {i}/{iterates}")
 
-        # alice_str_over_time.append(players['Alice'].strategy)
-        # bob_str_over_time.append(players['Bob'].strategy)
-        # charlie_str_over_time.append(players['Charlie'].strategy)
+            gradient_descent_ascent(G,players, lamda, total_gas_bound, use_max_lambda=use_max_lambda,
+                                    primal_step=primal_step, dual_step=dual_step)
 
-        # Tracking differences between distributions
-        # plt_kl_Alice.append(players['Alice'].kldiv)
-        # plt_kl_Bob.append(players['Bob'].kldiv)
-        # plt_kl_Charlie.append(players['Charlie'].kldiv)
+            # alice_str_over_time.append(players['Alice'].strategy)
+            # bob_str_over_time.append(players['Bob'].strategy)
+            # charlie_str_over_time.append(players['Charlie'].strategy)
 
-        # plt_wass_Alice.append(players['Alice'].wasser)
-        # plt_wass_Bob.append(players['Bob'].wasser)
-        # plt_wass_Charlie.append(players['Charlie'].wasser)
+            # Tracking differences between distributions
+            # plt_kl_Alice.append(players['Alice'].kldiv)
+            # plt_kl_Bob.append(players['Bob'].kldiv)
+            # plt_kl_Charlie.append(players['Charlie'].kldiv)
 
-    fig, axs = plt.subplots(1, 3, figsize=(12,5))
+            # plt_wass_Alice.append(players['Alice'].wasser)
+            # plt_wass_Bob.append(players['Bob'].wasser)
+            # plt_wass_Charlie.append(players['Charlie'].wasser)
 
-    bars = ['P1', 'P2', 'P3', 'P4', 'HW']
-    y_pos = np.arange(len(bars))
+        fig, axs = plt.subplots(1, 3, figsize=(12,5))
 
-    for i, player_name in enumerate(player_names):
-        players[player_name].plot_history_at('strategy', -1, axs[i], color=['navy', 'navy', 'navy', 'navy', 'purple'])
-        axs[i].set_xlabel('Action')
-    axs[0].set_ylabel('Probability')
+        bars = ['P1', 'P2', 'P3', 'P4', 'HW']
+        y_pos = np.arange(len(bars))
 
-    # axs[0].bar(range(len(alice_str_over_time[-1])), alice_str_over_time[-1], color=['navy', 'navy', 'navy', 'navy', 'purple'])
-    # axs[0].set_title('Alice')
-    # axs[0].set_xlabel('Action')
-    # axs[0].set_ylabel('Probability')
-    # axs[0].set_ylim([0, 1])
-    #
-    #
-    # axs[1].bar(range(len(bob_str_over_time[-1])), bob_str_over_time[-1], color=['navy', 'navy', 'navy', 'navy', 'purple'])
-    # axs[1].set_title('Bob')
-    # axs[1].set_xlabel('Action')
-    # axs[1].set_ylim([0, 1])
-    #
-    #
-    # axs[2].bar(range(len(charlie_str_over_time[-1])), charlie_str_over_time[-1], color=['navy', 'navy', 'navy', 'navy', 'purple'])
-    # axs[2].set_title('Charlie')
-    # axs[2].set_xlabel('Action')
-    # axs[2].set_ylim([0, 1])
+        for i, player_name in enumerate(player_names):
+            players[player_name].plot_history_at('strategy', -1, axs[i], color=['navy', 'navy', 'navy', 'navy', 'purple'])
+            axs[i].set_xlabel('Action')
+        axs[0].set_ylabel('Probability')
 
-    for ax in axs:
-        ax.axhline(y=0.25, color='gray', linestyle='--')
+        # axs[0].bar(range(len(alice_str_over_time[-1])), alice_str_over_time[-1], color=['navy', 'navy', 'navy', 'navy', 'purple'])
+        # axs[0].set_title('Alice')
+        # axs[0].set_xlabel('Action')
+        # axs[0].set_ylabel('Probability')
+        # axs[0].set_ylim([0, 1])
+        #
+        #
+        # axs[1].bar(range(len(bob_str_over_time[-1])), bob_str_over_time[-1], color=['navy', 'navy', 'navy', 'navy', 'purple'])
+        # axs[1].set_title('Bob')
+        # axs[1].set_xlabel('Action')
+        # axs[1].set_ylim([0, 1])
+        #
+        #
+        # axs[2].bar(range(len(charlie_str_over_time[-1])), charlie_str_over_time[-1], color=['navy', 'navy', 'navy', 'navy', 'purple'])
+        # axs[2].set_title('Charlie')
+        # axs[2].set_xlabel('Action')
+        # axs[2].set_ylim([0, 1])
 
-    #plt.ylim([0, 1])
+        for ax in axs:
+            ax.axhline(y=0.25, color='gray', linestyle='--')
 
-    plt.setp(axs, xticks=y_pos, xticklabels=bars)
-    plt.subplots_adjust(left=0.1, right=0.9, bottom = 0.2)
-    fig.text(0.5, 0.05, f'Total gas constraint: {knob}', ha='center', fontsize='14')
-    plt.savefig('experiment_result_{}.png'.format(total_gas_bound))
-    plt.close()
+        #plt.ylim([0, 1])
 
-    #save_animation(alice_str_over_time,'Alice')
-    #save_animation(bob_str_over_time,'Bob')
-    #save_animation(charlie_str_over_time,'Charlie')
+        plt.setp(axs, xticks=y_pos, xticklabels=bars)
+        plt.subplots_adjust(left=0.1, right=0.9, bottom = 0.2)
+        fig.text(0.5, 0.05, f'Total gas constraint: {knob}', ha='center', fontsize='14')
+        plt.savefig('experiment_result_{}.png'.format(total_gas_bound))
+        plt.close()
 
-    # plt.plot(plt_kl_Alice)
-    # plt.plot(plt_kl_Bob)
-    # plt.plot(plt_kl_Charlie)
-    # plt.savefig('probs.jpg')
-    # plt.close()
+        #save_animation(alice_str_over_time,'Alice')
+        #save_animation(bob_str_over_time,'Bob')
+        #save_animation(charlie_str_over_time,'Charlie')
 
-    # plt.plot(plt_wass_Alice)
-    # plt.plot(plt_wass_Bob)
-    # plt.plot(plt_wass_Charlie)
-    # plt.savefig('probs_wasser.jpg')
-    # plt.close()
+        # plt.plot(plt_kl_Alice)
+        # plt.plot(plt_kl_Bob)
+        # plt.plot(plt_kl_Charlie)
+        # plt.savefig('probs.jpg')
+        # plt.close()
 
-    # for player in players.values():
-    #     player.render_path()
+        # plt.plot(plt_wass_Alice)
+        # plt.plot(plt_wass_Bob)
+        # plt.plot(plt_wass_Charlie)
+        # plt.savefig('probs_wasser.jpg')
+        # plt.close()
 
-    #for name, player in players.items():
-    #  print("Player: ,",name,": Congestion: ",player.congestion)
-    #  print(player.path)
-    #  print(player.strategy)
+        # for player in players.values():
+        #     player.render_path()
+
+        #for name, player in players.items():
+        #  print("Player: ,",name,": Congestion: ",player.congestion)
+        #  print(player.path)
+        #  print(player.strategy)
