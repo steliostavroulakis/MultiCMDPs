dc0fd64b590b3df6f23ec753400f7dad1fd4f6c0
RogerMin <rogermingaming@gmail.com>
2023-05-12 07:07:20 +0800

    Unittestlized for easier tryout

--- DAG/dag_experiment.py
+++ DAG/dag_experiment.py
@@ -1,16 +1,21 @@
 from dag_train import train_dag
 from lib import create_dag
+import unittest
 
-G = create_dag()
 
-player_names = ['Alice', 'Bob', 'Charlie']
-player_constrains = [6, 6, 14]
-player_max_lambdas = [10, 10, 10]
-use_max_lambda = False
+class DAGExperiments(unittest.TestCase):
+    G = create_dag()
 
-iterates = 1000
-primal_step = 0.005
-dual_step = 0.1
+    def experiment_0(self):
+        player_names = ['Alice', 'Bob', 'Charlie']
+        player_constrains = [6, 6, 14]
+        player_max_lambdas = [10, 10, 10]
+        use_max_lambda = False
 
-train_dag(player_names=player_names, G=G, player_constrains=player_constrains, player_max_lambdas=player_max_lambdas,
-          iterates=iterates, primal_step=primal_step, dual_step=dual_step, pic_name="test")
+        iterates = 1000
+        primal_step = 0.005
+        dual_step = 0.1
+
+        train_dag(player_names=player_names, G=G, player_constrains=player_constrains,
+                  player_max_lambdas=player_max_lambdas,iterates=iterates, primal_step=primal_step, dual_step=dual_step,
+                  pic_name="test")
